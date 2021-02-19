8a5bfdffd03249059d3cce417d7ab9933ec4433c
Jordan <Jordan.Brockopp@target.com>
2021-02-19 14:25:39 -0600

    refactor: connect directly to database

---
+++ migrations/v0.7/database.go
@@ -0,0 +1,83 @@
+// Copyright (c) 2021 Target Brands, Inc. All rights reserved.
+//
+// Use of this source code is governed by the LICENSE file in this repository.
+
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/go-vela/server/database"
+	"github.com/go-vela/types/constants"
+
+	"github.com/sirupsen/logrus"
+	"github.com/urfave/cli/v2"
+)
+
+// connection represents all connection related
+// information used to communicate with the
+// provided database.
+type connection struct {
+	Idle int
+	Life time.Duration
+	Open int
+}
+
+// db represents all database related
+// information used to communicate
+// with the database.
+type db struct {
+	Driver     string
+	Config     string
+	Connection *connection
+
+	Client database.Service
+}
+
+// New creates a client with the provided database.
+func (d *db) New(c *cli.Context) error {
+	logrus.Debug("creating database from provided configuration")
+
+	switch d.Driver {
+	case constants.DriverPostgres:
+		// creating database from provided configuration
+		_database, err := database.New(c)
+		if err != nil {
+			return err
+		}
+
+		// update client with database service
+		d.Client = _database
+	case constants.DriverSqlite:
+		// creating database from provided configuration
+		_database, err := database.New(c)
+		if err != nil {
+			return err
+		}
+
+		// update client with database service
+		d.Client = _database
+	default:
+		return fmt.Errorf("invalid database driver: %s", d.Driver)
+	}
+
+	return nil
+}
+
+// Validate verifies the provided database is configured properly.
+func (d *db) Validate() error {
+	logrus.Info("validating provided database configuration")
+
+	// check if the database driver is set
+	if len(d.Driver) == 0 {
+		return fmt.Errorf("VELA_DATABASE_DRIVER is not properly configured")
+	}
+
+	// check if the database configuration is set
+	if len(d.Config) == 0 {
+		return fmt.Errorf("VELA_DATABASE_CONFIG is not properly configured")
+	}
+
+	return nil
+}
--- migrations/v0.7/main.go
+++ migrations/v0.7/main.go
@@ -5,93 +5,94 @@
 package main
 
 import (
-	"fmt"
 	"os"
+	"time"
 
-	"github.com/go-vela/sdk-go/vela"
 	"github.com/sirupsen/logrus"
+	"github.com/urfave/cli/v2"
+
+	_ "github.com/joho/godotenv/autoload"
 )
 
-// server represents the information you need to
-// interact with a Vela server
-type server struct {
-	// information about server
-	Addr  string
-	Token string
-
-	// clients for interaction
-	vela *vela.Client
-}
-
 func main() {
-	// set the log level for the utility
-	switch os.Getenv("VELA_LOG_LEVEL") {
-	case "t", "trace", "Trace", "TRACE":
-		logrus.SetLevel(logrus.TraceLevel)
-	case "d", "debug", "Debug", "DEBUG":
-		logrus.SetLevel(logrus.DebugLevel)
-	case "w", "warn", "Warn", "WARN":
-		logrus.SetLevel(logrus.WarnLevel)
-	case "e", "error", "Error", "ERROR":
-		logrus.SetLevel(logrus.ErrorLevel)
-	case "f", "fatal", "Fatal", "FATAL":
-		logrus.SetLevel(logrus.FatalLevel)
-	case "p", "panic", "Panic", "PANIC":
-		logrus.SetLevel(logrus.PanicLevel)
-	case "i", "info", "Info", "INFO":
-		fallthrough
-	default:
-		logrus.SetLevel(logrus.InfoLevel)
+	app := cli.NewApp()
+
+	// Utility Information
+
+	app.Name = "vela-migration"
+	app.HelpName = "vela-migration"
+	app.Usage = "Vela utility used to migrate from v0.6.x to v0.7.x"
+	app.Copyright = "Copyright (c) 2021 Target Brands, Inc. All rights reserved."
+	app.Authors = []*cli.Author{
+		{
+			Name:  "Vela Admins",
+			Email: "vela@target.com",
+		},
 	}
 
-	s := server{
-		Addr:  os.Getenv("VELA_ADDR"),
-		Token: os.Getenv("VELA_TOKEN"),
+	// Utility Metadata
+
+	app.Action = run
+	app.Compiled = time.Now()
+	app.Version = "v1.0.0"
+
+	// Utility Flags
+
+	app.Flags = []cli.Flag{
+
+		// Database Flags
+
+		&cli.StringFlag{
+			EnvVars: []string{"VELA_DATABASE_DRIVER", "DATABASE_DRIVER"},
+			Name:    "database.driver",
+			Usage:   "sets the driver to be used for the database",
+			Value:   "sqlite3",
+		},
+		&cli.StringFlag{
+			EnvVars: []string{"VELA_DATABASE_CONFIG", "DATABASE_CONFIG"},
+			Name:    "database.config",
+			Usage:   "sets the configuration string to be used for the database",
+			Value:   "vela.sqlite",
+		},
+		&cli.IntFlag{
+			EnvVars: []string{"VELA_DATABASE_CONNECTION_OPEN", "DATABASE_CONNECTION_OPEN"},
+			Name:    "database.connection.open",
+			Usage:   "sets the number of open connections to the database",
+			Value:   0,
+		},
+		&cli.IntFlag{
+			EnvVars: []string{"VELA_DATABASE_CONNECTION_IDLE", "DATABASE_CONNECTION_IDLE"},
+			Name:    "database.connection.idle",
+			Usage:   "sets the number of idle connections to the database",
+			Value:   2,
+		},
+		&cli.DurationFlag{
+			EnvVars: []string{"VELA_DATABASE_CONNECTION_LIFE", "DATABASE_CONNECTION_LIFE"},
+			Name:    "database.connection.life",
+			Usage:   "sets the amount of time a connection may be reused for the database",
+			Value:   30 * time.Minute,
+		},
+
+		// Logger Flags
+
+		&cli.StringFlag{
+			EnvVars: []string{"VELA_LOG_FORMAT", "LOG_FORMAT"},
+			Name:    "log.format",
+			Usage:   "set log format for the utility",
+			Value:   "json",
+		},
+		&cli.StringFlag{
+			EnvVars: []string{"VELA_LOG_LEVEL", "LOG_LEVEL"},
+			Name:    "log.level",
+			Usage:   "set log level for the utility",
+			Value:   "info",
+		},
 	}
 
-	logrus.Info("validating server configuration")
-	// validate provided server configuration
-	err := s.validate()
+	// Utility Start
+
+	err := app.Run(os.Args)
 	if err != nil {
 		logrus.Fatal(err)
 	}
-
-	logrus.Info("creating Vela client from server configuration")
-	// create new client from provided server configuration
-	err = s.new()
-	if err != nil {
-		logrus.Fatal(err)
-	}
-}
-
-// validate the server information
-func (s *server) validate() error {
-	// check if the addr is set
-	if len(s.Addr) == 0 {
-		return fmt.Errorf("VELA_ADDR is not properly configured")
-	}
-
-	// check if the token is set
-	if len(s.Token) == 0 {
-		return fmt.Errorf("VELA_TOKEN is not properly configured")
-	}
-
-	return nil
-}
-
-// new creates an interactable client for the server
-func (s *server) new() error {
-	// Create a new vela client for interacting with server
-	c, err := vela.NewClient(s.Addr, "", nil)
-	if err != nil {
-		return fmt.Errorf("unable to create client: %w", err)
-	}
-
-	// Set new token in existing client
-	c.Authentication.SetTokenAuth(s.Token)
-
-	// add the client to the server
-	s.vela = c
-
-	return nil
 }
