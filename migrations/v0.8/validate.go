491fc0c468f88a91d4b8467eed6a00a485274a36
Neal <neal.coleman@target.com>
2021-05-25 09:30:06 -0500

    feat(repo): add repo counter sync with build number (#264)

---
+++ migrations/v0.8/validate.go
@@ -0,0 +1,56 @@
+// Copyright (c) 2021 Target Brands, Inc. All rights reserved.
+//
+// Use of this source code is governed by the LICENSE file in this repository.
+
+package main
+
+import (
+	"fmt"
+
+	"github.com/go-vela/types/constants"
+
+	"github.com/sirupsen/logrus"
+)
+
+// Validate verifies the provided database is configured properly.
+func (d *db) Validate() error {
+	logrus.Debug("validating provided database configuration")
+
+	// check if an action was provided
+	switch {
+	case d.Actions.AlterTables:
+		fallthrough
+	case d.Actions.SyncCounter:
+		fallthrough
+	case d.Actions.All:
+		break
+	default:
+		logrus.Warning("no vela-migration actions provided")
+	}
+
+	// check if the database driver is set
+	if len(d.Driver) == 0 {
+		return fmt.Errorf("VELA_DATABASE_DRIVER is not properly configured")
+	}
+
+	switch d.Driver {
+	case constants.DriverPostgres:
+		fallthrough
+	case constants.DriverSqlite:
+		break
+	default:
+		return fmt.Errorf("invalid VELA_DATABASE_DRIVER provided: %s", d.Driver)
+	}
+
+	// check if the database configuration is set
+	if len(d.Config) == 0 {
+		return fmt.Errorf("VELA_DATABASE_CONFIG is not properly configured")
+	}
+
+	// check if the database concurrency limit is set
+	if d.ConcurrencyLimit < 1 {
+		return fmt.Errorf("VELA_CONCURRENCY_LIMIT is not properly configured")
+	}
+
+	return nil
+}
