bc469697b375d74cf6a42f3e5ee135bdf7365915
kneal <neal.coleman@target.com>
2021-07-21 11:54:56 -0500

    feat(v0.9): add initial migration framework

---
+++ migrations/v0.9/run.go
@@ -0,0 +1,47 @@
+// Copyright (c) 2021 Target Brands, Inc. All rights reserved.
+//
+// Use of this source code is governed by the LICENSE file in this repository.
+
+package main
+
+import (
+	"github.com/sirupsen/logrus"
+
+	"github.com/urfave/cli/v2"
+)
+
+// run executes the utility based
+// off the configuration provided.
+func run(c *cli.Context) error {
+	// set log format for the utility
+	switch c.String("log.format") {
+	case "t", "text", "Text", "TEXT":
+		logrus.SetFormatter(&logrus.TextFormatter{})
+	case "j", "json", "Json", "JSON":
+		fallthrough
+	default:
+		logrus.SetFormatter(&logrus.JSONFormatter{})
+	}
+
+	// set log level for the utility
+	switch c.String("log.level") {
+	case "t", "trace", "Trace", "TRACE":
+		logrus.SetLevel(logrus.TraceLevel)
+	case "d", "debug", "Debug", "DEBUG":
+		logrus.SetLevel(logrus.DebugLevel)
+	case "w", "warn", "Warn", "WARN":
+		logrus.SetLevel(logrus.WarnLevel)
+	case "e", "error", "Error", "ERROR":
+		logrus.SetLevel(logrus.ErrorLevel)
+	case "f", "fatal", "Fatal", "FATAL":
+		logrus.SetLevel(logrus.FatalLevel)
+	case "p", "panic", "Panic", "PANIC":
+		logrus.SetLevel(logrus.PanicLevel)
+	case "i", "info", "Info", "INFO":
+		fallthrough
+	default:
+		logrus.SetLevel(logrus.InfoLevel)
+	}
+
+	return nil
+}
