93f59bf704311f5c5eced7a1aa2916ea1f645011
Easton Crupper <65553218+ecrupper@users.noreply.github.com>
2023-04-25 10:51:48 -0600

    chore(release): migration script for adding repos table topics column (#786)
    * migration script for adding repos table topics column
    
    * update to v19 in Makefile and go mod
    
    * Update migrations/v0.19/main.go
    
    Co-authored-by: dave vader <48764154+plyr4@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: dave vader <48764154+plyr4@users.noreply.github.com>

---
+++ migrations/v0.19/database.go
@@ -0,0 +1,25 @@
+// Copyright (c) 2022 Target Brands, Inc. All rights reserved.
+//
+// Use of this source code is governed by the LICENSE file in this repository.
+
+package main
+
+import "gorm.io/gorm"
+
+// actions represents all potential actions
+// this utility can invoke with the database.
+type actions struct {
+	All         bool
+	AlterTables bool
+}
+
+// db represents all database related
+// information used to communicate
+// with the database.
+type db struct {
+	Driver  string
+	Address string
+
+	Actions *actions
+	Gorm    *gorm.DB
+}
---
+++ migrations/v0.19/main.go
@@ -0,0 +1,83 @@
+// Copyright (c) 2023 Target Brands, Inc. All rights reserved.
+//
+// Use of this source code is governed by the LICENSE file in this repository.
+
+package main
+
+import (
+	"os"
+	"time"
+
+	"github.com/go-vela/server/database"
+	"github.com/sirupsen/logrus"
+	"github.com/urfave/cli/v2"
+
+	_ "github.com/joho/godotenv/autoload"
+)
+
+func main() {
+	app := cli.NewApp()
+
+	// Utility Information
+
+	app.Name = "vela-migration"
+	app.HelpName = "vela-migration"
+	app.Usage = "Vela utility used to migrate from v0.18.x to v0.19.x"
+	app.Copyright = "Copyright (c) 2023 Target Brands, Inc. All rights reserved."
+	app.Authors = []*cli.Author{
+		{
+			Name:  "Vela Admins",
+			Email: "vela@target.com",
+		},
+	}
+
+	// Utility Metadata
+
+	app.Action = run
+	app.Compiled = time.Now()
+	app.Version = "v1.0.0"
+
+	// Utility Flags
+
+	app.Flags = []cli.Flag{
+
+		// Action Flags
+
+		&cli.BoolFlag{
+			EnvVars: []string{"VELA_ACTION_ALL", "ACTION_ALL"},
+			Name:    "action.all",
+			Usage:   "enables running all actions for v0.19.x",
+		},
+		&cli.BoolFlag{
+			EnvVars: []string{"VELA_ALTER_TABLES", "ALTER_TABLES"},
+			Name:    "alter.tables",
+			Usage:   "enables altering the table configuration for v0.19.x",
+		},
+
+		// Logger Flags
+
+		&cli.StringFlag{
+			EnvVars: []string{"VELA_LOG_FORMAT", "LOG_FORMAT"},
+			Name:    "log.format",
+			Usage:   "set log format for the utility",
+			Value:   "json",
+		},
+		&cli.StringFlag{
+			EnvVars: []string{"VELA_LOG_LEVEL", "LOG_LEVEL"},
+			Name:    "log.level",
+			Usage:   "set log level for the utility",
+			Value:   "info",
+		},
+	}
+
+	// Database Flags
+
+	app.Flags = append(app.Flags, database.Flags...)
+
+	// Utility Start
+
+	err := app.Run(os.Args)
+	if err != nil {
+		logrus.Fatal(err)
+	}
+}
