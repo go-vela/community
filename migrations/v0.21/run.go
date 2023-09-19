e42afff16509c5e101f942a8d953217c3a81358f
ecrupper <easton.crupper12@gmail.com>
2023-09-18 22:37:37 -0500

    chore(v0.21): add migration script

---
+++ migrations/v0.21/run.go
@@ -0,0 +1,63 @@
+// Copyright (c) 2023 Target Brands, Inc. All rights reserved.
+//
+// Use of this source code is governed by the LICENSE file in this repository.
+
+package main
+
+import (
+	"github.com/sirupsen/logrus"
+	"github.com/urfave/cli/v2"
+)
+
+// run executes the utility based
+// off the configuration provided.
+func run(c *cli.Context) error {
+	// set log format for the utility
+	switch c.String("log.format") {
+	case "t", "text", "Text", "TEXT":
+		logrus.SetFormatter(&logrus.TextFormatter{})
+	case "j", "json", "Json", "JSON":
+		fallthrough
+	default:
+		logrus.SetFormatter(&logrus.JSONFormatter{})
+	}
+
+	// set log level for the utility
+	switch c.String("log.level") {
+	case "t", "trace", "Trace", "TRACE":
+		logrus.SetLevel(logrus.TraceLevel)
+	case "d", "debug", "Debug", "DEBUG":
+		logrus.SetLevel(logrus.DebugLevel)
+	case "w", "warn", "Warn", "WARN":
+		logrus.SetLevel(logrus.WarnLevel)
+	case "e", "error", "Error", "ERROR":
+		logrus.SetLevel(logrus.ErrorLevel)
+	case "f", "fatal", "Fatal", "FATAL":
+		logrus.SetLevel(logrus.FatalLevel)
+	case "p", "panic", "Panic", "PANIC":
+		logrus.SetLevel(logrus.PanicLevel)
+	case "i", "info", "Info", "INFO":
+		fallthrough
+	default:
+		logrus.SetLevel(logrus.InfoLevel)
+	}
+
+	// create database object
+	d := &db{
+		Driver:  c.String("database.driver"),
+		Address: c.String("database.addr"),
+		Actions: &actions{
+			All:             c.Bool("action.all"),
+			AlterTables:     c.Bool("alter.tables"),
+			UpdateSchedules: c.Bool("update.schedules"),
+		},
+	}
+
+	// validate database configuration
+	err := d.Validate()
+	if err != nil {
+		return err
+	}
+
+	return d.Exec(c)
+}
