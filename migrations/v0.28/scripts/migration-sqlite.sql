593fd3dbe05df0f9efa1c8b4453e68d02bb6e735
Easton Crupper <65553218+ecrupper@users.noreply.github.com>
2026-04-03 15:05:58 -0500

    chore: add migration info for v0.28.x (#1083)
    * chore: add migration info for v0.28.x
    
    * add DML step

---
+++ migrations/v0.28/scripts/migration-sqlite.sql
@@ -0,0 +1,68 @@
+/*
+    VELA MIGRATION v0.27.x --> v0.28.x
+
+    Please note that this SQL file must be executed prior to upgrading Vela to version 0.28.x
+*/
+
+/*
+___  ___  _    
+|  \ |  \ |    
+|__/ |__/ |___ 
+*/
+
+-- Start table alter transaction
+BEGIN TRANSACTION;
+
+-- Add artifacts to pipelines table
+ALTER TABLE pipelines
+    ADD COLUMN IF NOT EXISTS artifacts BOOLEAN
+;
+
+-- Add merge_queue_events to repos table
+ALTER TABLE repos
+    ADD COLUMN IF NOT EXISTS merge_queue_events TEXT
+;
+
+-- Add hook_counter to repos table
+ALTER TABLE repos
+    ADD COLUMN IF NOT EXISTS hook_counter INTEGER
+;
+
+-- Add enable_shared_secrets to settings table
+ALTER TABLE settings
+    ADD COLUMN IF NOT EXISTS enable_shared_secrets BOOLEAN
+;
+
+-- Add enable_org_secrets to settings table
+ALTER TABLE settings
+    ADD COLUMN IF NOT EXISTS enable_org_secrets BOOLEAN
+;
+
+-- Add enable_repo_secrets to settings table
+ALTER TABLE settings
+    ADD COLUMN IF NOT EXISTS enable_repo_secrets BOOLEAN
+;
+
+-- Save changes
+COMMIT;
+
+/*
+___  _  _ _    
+|  \ |\/| |    
+|__/ |  | |___           
+*/
+
+-- Start transaction
+
+BEGIN TRANSACTION;
+
+-- Update hook_counter in repos table
+UPDATE repos
+SET hook_counter = COALESCE((
+    SELECT COUNT(*)
+    FROM hooks
+    WHERE hooks.repo_id = repos.id
+), 0);
+
+-- Save changes
+COMMIT;
